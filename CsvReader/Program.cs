c278f53dcc7c1b42980423d0a401602e49546845
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-11-08 10:01:52 +0300

    Add project files.

---
+++ CsvReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSVReaderNestedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Repos\CsvReaderList\CountryPopulations.csv";
+
+            Reader csvreader = new Reader(filepath);
+
+            List<Country> countries = csvreader.ReadAllCountries();
+            
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{Formatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            Console.WriteLine($"{countries.Count} countries");
+        }
+    }
+}
